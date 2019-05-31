4f747406bcfd5722b883c125490f9ea319d4af96
hcarneiro <hcarneiro@weboo.biz>
2019-05-31 13:57:33 +0100

    updates to run app on heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-const port = process.env.PORT || 80;
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+const port = process.env.PORT || 80
 
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-app.listen(port);
+app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
+app.listen(port)
